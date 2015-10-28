0375781f8328e3a72174d296a783b32af00cd53e
Kris Hicks <khicks@pivotal.io>
2015-10-28 15:19:26 -0700

    WIP

---
+++ ci/scripts/windows-build.bat
@@ -0,0 +1,21 @@
+set root=%CD%
+
+SET GOPATH=%CD%\gopath
+SET PATH=%PATH%;%CD%\gopath\bin
+
+cd gopath\src\github.com\cloudfoundry\cli
+
+SET GODEPSPATH=%CD%\Godeps\_workspace
+SET GOPATH=%GODEPSPATH%;%GOPATH%;
+SET PATH=%PATH%;%GODEPSPATH%\bin
+
+go get github.com/jteeuwen/go-bindata/... || exit /b 1
+go-bindata -pkg resources -ignore ".go" -o cf/resources/i18n_resources.go cf/i18n/resources/... cf/i18n/test_fixtures/... || exit /b 1
+
+powershell -command set-executionpolicy remotesigned < NUL || exit /b 1
+powershell .\bin\replace-sha.ps1 < NUL || exit /b 1
+go build -v -o %CF_EXE_NAME% ./main || exit /b 1
+
+cd %root%
+
+move /y gopath\src\github.com\cloudfoundry\cli\cf.exe cf-windows%BITS%-binary.exe
